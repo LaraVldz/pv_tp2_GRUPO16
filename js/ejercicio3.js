f2882a0fea1b0a455c21509dccda6c862c10b3a7
Ignacio Jes <ignaciojes@gmail.com>
2025-04-15 18:46:43 -0300

    Subo ejercicios 23 y 24

--- js/ejercicio3.js
+++
@@ -1,5 +0,0 @@
-/* Calcular y mostrar por consola el area de un circulo cuyo radio es 7,5 */
-const pi = 3.14;
-let radio=7.5;
-const cal_area = (radio) => pi*(radio**2) ;
-console.log('Calculo del area: ' + cal_area(radio))
